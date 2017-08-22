b961dacd61f55282e6569fae088046aa550f1137
Dmitry Kutsenko <d2emonium@gmail.com>
2017-08-22 16:53:32 +0300

    Updated config

--- src/config.py
+++ src/config.py
@@ -17,8 +17,8 @@ class Config(object):
     TESTING = False
     SECRET_KEY = "ThereIsNoSpoon"
 
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///db/execom.db'
+    # USERNAME = 'admin'
+    # PASSWORD = 'admin'
 
     LOG = {
         "FILENAME": os.path.join(BASE_DIR, "log", "execom.log"),
@@ -27,11 +27,14 @@ class Config(object):
         "FORMAT": "%(asctime)s[%(levelname)s]:\t%(message)s\tin %(module)s at %(lineno)d",
     }
 
+    # configuration db
     DB_PATH = os.path.join(BASE_DIR, "db")
     DB_FILENAME = "blog.db"
     SQLALCHEMY_DATABASE_URI = "sqlite:////%s/%s" % (DB_PATH, DB_FILENAME)
     # SQLALCHEMY_DATABASE_URI = "sqlite:///../db/%s" % (DB_FILENAME)
     # SQLALCHEMY_DATABASE_URI = "sqlite:///db/%s" % (DB_FILENAME)
+    # SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s/%s" % ('root', 'root', '127.0.0.1', 'pythonpub1')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     BACKUP_TIME = timedelta(minutes=30)
     BACKUP_PATH = os.path.join(BASE_DIR, "db", "backup")
@@ -40,8 +43,15 @@ class Config(object):
     VIEW_CASE = "edit_case"
 
     UPLOAD_PATH = os.path.join(BASE_DIR, "upload")
+    # UPLOAD_FOLDER = './static/upload/'
+    # ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
+    # configuration page num
     RECORDS_ON_PAGE = 50
+    # PER_PAGE = 10
+
+    # RECAPTCHA_PUBLIC_KEY = '6Ld4rwITAAAAAKUD5AntlHi7HL36W2vHJQOIjQmA'
+    # RECAPTCHA_PRIVATE_KEY = '6Ld4rwITAAAAAFE8nTS852QbsqCBx1mN8D4BqenE'
 
 
 class ProductionConfig(Config):
