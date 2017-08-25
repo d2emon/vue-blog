b62dbc6b2d0cb28cbc27aaf0904cd6b5a2363591
Dmitry Kutsenko <d2emonium@gmail.com>
2017-08-25 11:13:34 +0300

    Index page template

--- src/views.py
+++ src/views.py
@@ -4,9 +4,9 @@ from app import app
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('error/404.html'), 404
 
 
 @app.route('/error/404')
 def error_404():
-    return render_template('404.html'), 404
+    return render_template('error/404.html'), 404
