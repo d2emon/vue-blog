bd16b3e7e0e48dd6118f6e63726cc6fd714a4d8a
Dmitry Kutsenko <d2emonium@gmail.com>
2017-08-25 22:50:56 +0300

    Category page

--- src/blog/views.py
+++ src/blog/views.py
@@ -7,7 +7,7 @@ import random
 
 
 def page():
-    page_str = request.args.get('page')
+    page_str = request.args.get('page_id')
     try:
         page = int(page_str)
     except (ValueError, TypeError):
@@ -46,6 +46,43 @@ def index(page_id=1):
         # comments=comments,
 
         nav_current="index"
+    )
+
+
+@app.route('/category/<int:cat_id>')
+@app.route('/category/<int:cat_id>/page/<int:page_id>')
+def category(cat_id=1, page_id=1):
+    # categories = Category.query.getall()
+    categories = Category.query.all()
+
+    # hot = Post.query.hottest()[:20]
+    hot = Post.query.all()[:20]
+    # new = Post.query.newpost()[:20]
+    new = Post.query.all()[:20]
+
+    # tag = Tag.query.getall()
+    tag = Tag.query.all()
+    random.shuffle(tag)
+    tag = tag[:20]
+
+    # comments = Comment.query.newcomment()[:20]
+
+    cat = Category.query.get_or_404(cat_id)
+
+    # p = pageby(cat.posts, page_id, per_page, Post.created_at.desc())
+    articles = cat.posts.paginate(page(), app.config.get('RECORDS_ON_PAGE'))
+
+    return render_template(
+        '/index/category.html',
+        id=cat_id,
+        cat=cat,
+        categories=categories,
+        articles=articles,
+        hotarticles=hot,
+        newpost=new,
+        tags=tag,
+        # comments=comments,
+        page_id=page_id,
     )
 
 
